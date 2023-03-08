325e91d5a976645a43a159dd539109120a07290a
Taiyousan <shamsbenhamou@gmail.com>
2023-03-08 15:09:00 +0100

    repartition cartes et couleurs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,55 @@
-<script setup></script>
+<script setup>
+import CardGame from './components/CardGame.vue';
+import wordslist from './assets/data/wordslist.json';
+import couleurslist from './assets/data/couleurs.json';
 
-<template></template>
+let random = wordslist.sort(() => .5 - Math.random()).slice(0,25)
+// Je récupère la liste de mots et j'en tire 25 que je mets dans un array 'random'
+const CardCouleurs = couleurslist.sort((a, b) => 0.5 - Math.random());
+console.log(CardCouleurs)
 
-<style scoped></style>
+const newRandom = random.map((word, index) => {
+    return  {
+        couleur: CardCouleurs[index],
+        mot: word
+    }
+})
+// Je crée un nouvel array 'newRandom' qui contient les mots de 'random' et une couleur
+
+function exportToJsonFile(jsonData) {
+    let dataStr = JSON.stringify(jsonData);
+    let dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(dataStr);
+
+    let exportFileDefaultName = 'data.json';
+
+    let linkElement = document.createElement('a');
+    linkElement.setAttribute('href', dataUri);
+    linkElement.setAttribute('download', exportFileDefaultName);
+    linkElement.click();
+}
+
+</script>
+
+<template>
+    <div class="app">
+        <div class="plateau">
+            <!-- <CardGame couleur="blue" mot="chinchilla"/> -->
+            <template v-for="word in newRandom">
+               <CardGame  :mot=word.mot :couleur=word.couleur />
+            </template>
+
+        </div>
+    </div>
+
+    <button v-on:click=exportToJsonFile(newRandom)>télacharger plan</button>
+</template>
+
+<style scoped>
+.plateau {
+    border: 1px solid black;
+    width: 60vw;
+    display : grid;
+    grid-template-columns: repeat(5, 15%);
+    grid-template-rows: repeat(5, 80px);
+}
+</style>
--- src/components/CardGame.vue
+++ src/components/CardGame.vue
@@ -1,5 +1,48 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
 
-<template></template>
+const props = defineProps(['couleur', 'mot'])
 
-<style scoped></style>
+const motCard = props.mot.toUpperCase();
+console.log(props.couleur)
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-mot-container">
+            <div class="card-mot">
+                <p>{{motCard}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 120px;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: end;
+    padding-bottom: 15px;
+    border-radius: 5px;
+    
+    background-color: v-bind($props.couleur);
+}
+
+.card-mot-container {
+    height: 20%;
+    width: 80%;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-mot-container p {
+    font-size: 60%;
+    font-weight: bold;
+    text-align: center;
+    margin: 0;
+}
+</style>
