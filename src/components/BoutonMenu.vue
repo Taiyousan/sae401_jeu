b9e6f562c2886e18b739d844df688e80b15eb855
Taiyousan <shamsbenhamou@gmail.com>
2023-03-16 12:23:29 +0100

    design

---
+++ src/components/BoutonMenu.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+const props = defineProps(['content','variant', 'link'])
+
+
+
+</script>
+
+<template>
+    <a href="">
+        <div class="bouton-menu">
+            <div class="bouton-menu__texte">{{props.content}}{{ props.variant }}</div>
+        </div>
+    </a>
+</template>
+
+<style scoped>
+
+a {
+    text-decoration: none;
+}
+
+.bouton-menu {
+    width: fit-content;
+    height: 50px;
+    background-color: #F2F2F2;
+    border-radius: 8%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.3s ease;
+    padding: 0 10px 0 10px;
+}
+
+.bouton-menu:hover {
+    transform: scale(1.1);
+}
+
+.bouton-menu__texte {
+    font-size: 16px;
+    font-weight: bold;
+    color: #000000;
+    text-transform: uppercase;
+}
+</style>
--- src/views/PdvJoueur.vue
+++ src/views/PdvJoueur.vue
@@ -1,6 +1,7 @@
 <script setup>
 import CardGame from '../components/CardGame.vue';
 import CardGrille from '../components/CardGrille.vue';
+import BoutonMenu from '../components/BoutonMenu.vue';
 import wordslist from '../assets/data/wordslist.json';
 import couleurslist from '../assets/data/couleurs.json';
 import couleurslistj2 from '../assets/data/couleursj2.json';
@@ -39,6 +40,22 @@ function exportToJsonFile(jsonData) {
     linkElement.setAttribute('download', exportFileDefaultName);
     linkElement.click();
 }
+const bouton1 = [
+    'Joueurs : ',
+    '2',
+    'lien'
+]
+const boutonRegles = [
+    'Règles',
+    'lien'
+]
+const boutonReset = [
+    'Réinitialiser',
+    'lien'
+]
+
+const joueurConnecte = 'Michel';
+
 
 
 //Je créé une fonction qui sera appelée par le bouton pour exporter le plan de jeu au format json
@@ -49,19 +66,35 @@ function exportToJsonFile(jsonData) {
 
 <template>
     <div class="app">
-        
+        <div class="app-nav">
+            <div class="app-nav-content app-nav-content1">
+                <BoutonMenu :content=bouton1[0] :variant=bouton1[1] :link=bouton1[2] />
+                <BoutonMenu :variant=joueurConnecte :link=bouton1[2] />
+            </div>
+            <div class="app-nav-content app-nav-content2">
+                <BoutonMenu :content=boutonRegles[0] :link=boutonRegles[2] />
+                <BoutonMenu :content=boutonReset[0] :link=boutonReset[2] />
+            </div>
+        </div>
            
         <div class="joueur-container">
-            <div class="plateau">
-                <template v-for="word in newRandom">
-                   <CardGame :mot=word.mot :couleur=word.couleurJ1 :opponentCouleur=word.couleurJ2 :position=word.position :joueur=1 />
-                   <!-- // Je passe les props 'mot' et 'couleur' à mon composant CardGame avec une couleur spécifique au joueur 1 -->
-                </template>
+            <div class="joueur-content-left">
+
             </div>
-            <div class="grille" >
-                <span v-for="word in newRandom">
-                    <CardGrille :couleur=word.couleurJ1 />
-                </span>
+            <div class="joueur-content-center">
+                <div class="plateau">
+                    <template v-for="word in newRandom">
+                       <CardGame :mot="word.mot" :couleur=word.couleurJ1 :opponentCouleur=word.couleurJ2 :position=word.position :joueur=1 />
+                       <!-- // Je passe les props 'mot' et 'couleur' à mon composant CardGame avec une couleur spécifique au joueur 1 -->
+                    </template>
+                </div>
+            </div>
+            <div class="joueur-content-right">
+                <div class="grille" >
+                    <span v-for="word in newRandom">
+                        <CardGrille :couleur=word.couleurJ1 />
+                    </span>
+                </div>
             </div>
         </div>
         
@@ -72,12 +105,15 @@ function exportToJsonFile(jsonData) {
 
 <style scoped>
 .plateau {
-    /* border: 8px solid black; */
-    width: 60vw;
+    border: 8px solid black;
+    height: 80%;
+    width: 100%;
     display : grid;
-    grid-template-columns: repeat(5, 15%);
+    grid-template-columns: repeat(5, 20%);
     grid-template-rows: repeat(5, 80px);
+    gap: 10px;
     margin: 10px;
+    /* background-color: aquamarine; */
 }
 
 .plateauJ2 {
@@ -95,11 +131,56 @@ function exportToJsonFile(jsonData) {
     display : grid;
     grid-template-columns: repeat(5, 25%);
     grid-template-rows: repeat(5, 20%);
+   
 }
 
 .joueur-container {
     display: flex;
     flex-direction: row;
     align-items: center;
+    /* background-color: rgb(211, 237, 237); */
+    border: 1px solid black;
+    height: 90vh;
+}
+
+.joueur-content-left {
+    width: 25%;
+    height: 100%;
+}
+
+.joueur-content-center {
+    height: 100%;
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+
+}
+
+.joueur-content-right {
+    width: 25%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.app-nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin: 10px;
+    padding-left: 5%;
+    padding-right: 5%;
+}
+
+.app-nav-content {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 20%;
 }
 </style>
