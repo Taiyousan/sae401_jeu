bc58307b8a03b54abc9122a0471413de0f23a24c
Taiyousan <shamsbenhamou@gmail.com>
2023-03-18 19:28:57 +0100

    index

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,16 @@ import Router from './router/index.js';
 
 
 </script>
-
+oui
 <template>
     <div class="app">
-      <!-- <nav>
+      <nav>
         <router-link to="/">Home</router-link> |
         <router-link to="/jeu">Jeu</router-link> |
         <router-link to="/partiesaved">Partie Saved</router-link> |
         <router-link to="/prototype">Protoype de jeu</router-link> |
         <router-link to="/pdvjoueur">Point de vue Joueur</router-link>
-      </nav> -->
+      </nav>
     </div>
     <router-view></router-view>
 </template>
